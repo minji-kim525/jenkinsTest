a460d51c0413dbdba995ca3613be14419d99fb02
minji <alswlwkd20@naver.com>
2022-07-04 23:03:39 +0900

    test

---
+++ src/main/java/com/sparta/jenkinstest/HelloController.java
@@ -0,0 +1,11 @@
+package com.sparta.jenkinstest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HelloController {
+
+    @GetMapping
+    public String sayHello(){
+        return "Hello, Jenkins!!!!";
+    }
+}
